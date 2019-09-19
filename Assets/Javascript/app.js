cec0e3d6c16de8713978b47dd02d64f1d7339ec0
jackellis520 <jack.ellis520@gmail.com>
2019-09-19 18:47:21 -0500

    added and changed lots of things still a bit to go

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,9 +1,49 @@
-$("#gif-portal").on("click", function() {
+var gifName = ["bees", "red", "blue"];
+// loop through an array of gif names
+// create a button for each name
+// add buttons to the screen
+// on.click ajax call
+// method.push(add another element to the array)
+for (var i = 0; i < gifName.length; i++) {
+    var button = $("<button>");
+    button.addClass("gif-button");
+    button.attr("data-name", gifName[i]);
+    button.text(gifName[i]);
+
+    var buttonDiv = $("#button");
+    buttonDiv.append(button);
+}
+// pass the results.data into the function
+// for loops, arrays, and function calls
+$(".gif-button").on("click", function(){
+    search
+    ajax();
+})
+
+$("#gif-portal").on("click", function(){  
+
+    ajax();
+})
+
+            $(".gif").on("click", function() {
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                
+                    $(this).attr("src", $(this).attr("data-animated"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                   
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still")
+                }
+
+            })
+
+  function ajax() {
+
     var apikey = "&api_key=zXesKMJjcJyLGyXZLXDS1un9SLAT2hTm&limit=5"
     var searchResults = $("#search").val().trim();
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchResults + apikey;
-
-
 
     $.ajax({
         url: queryURL,
@@ -20,32 +60,14 @@ $("#gif-portal").on("click", function() {
             // <img />
             gifImage.attr("src", results[i].images.fixed_height_still.url);
             gifImage.addClass("gif");
-            // <img src="giphy.com/still" />
+        
             gifImage.attr("data-still", results[i].images.fixed_height_still.url);
             gifImage.attr("data-animated", results[i].images.fixed_height.url);
             gifImage.attr("data-state", "still");
-//console.log(gifImage);
-            $(".gif").on("click", function() {
-                var state = $(this).attr("data-state");
-          //      console.log(state);
-                if (state === "still") {
-                
-                    $(this).attr("src", $(this).attr("data-animated"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                   
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still")
-                }
-
-            })
-
-
 
             gifDiv.append(gifImage);
 
             $("#gifDiv").prepend(gifImage);
-        }
-
+        };
     });
-});
+};
